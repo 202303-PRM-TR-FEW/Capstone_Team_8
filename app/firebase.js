14763249308e99f70a791d9fba8d1ea47be2a71f
alwaysalty <cenkerdonmez@gmail.com>
2023-06-28 20:13:16 +0300

    Feat
    Firebase Added

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8UTCbWS3P2FHDpLXFG07yTQmalAa8EHw",
+  authDomain: "crowdfundingcapstone.firebaseapp.com",
+  projectId: "crowdfundingcapstone",
+  storageBucket: "crowdfundingcapstone.appspot.com",
+  messagingSenderId: "312824414413",
+  appId: "1:312824414413:web:ac68318b2bee9fc4dc6984",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
