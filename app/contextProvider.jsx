b82e72e583d9c63235b385f1c0acdee35417d5e2
alwaysalty <cenkerdonmez@gmail.com>
2023-07-08 20:49:29 +0300

    fixed errors

--- app/contextProvider.jsx
+++ app/contextProvider.jsx
@@ -1,8 +1,8 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { onAuthStateChanged, getAuth } from 'firebase/auth';
-import app from '../firebase/firebase';
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
+import app from "../firebase/firebase";
+import { useRouter } from "next/navigation";
 const auth = getAuth(app);
 
 export const AuthContext = React.createContext({});
@@ -10,25 +10,25 @@ export const AuthContext = React.createContext({});
 export const useAuthContext = () => React.useContext(AuthContext);
 
 export const AuthContextProvider = ({ children }) => {
-	const auth = getAuth();
-	const [user, setUser] = useState(false);
-	const [loading, setLoading] = useState(true);
-	const router = useRouter();
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-			if (!user) {
-				setUser(false);
-				router.push('/login');
-			} else {
-				setUser(true);
-			}
-			setLoading(false);
-		});
+  const auth = getAuth();
+  const [user, setUser] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        setUser(false);
+        router.push("/login");
+      } else {
+        setUser(true);
+      }
+      setLoading(false);
+    });
 
-		return () => unsubscribe();
-	}, []);
+    return () => unsubscribe();
+  }, [auth, router]);
 
-	return (
-		<AuthContext.Provider value={{ user }}> {children}</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider value={{ user }}> {children}</AuthContext.Provider>
+  );
 };
