e7b670893eec98a3905f8ec444489a5630c42b06
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-07-11 16:48:16 +0300

    profilepagecreated

---
+++ app/profile/page.jsx
@@ -0,0 +1,21 @@
+import PageLayout from "@/components/pageLayout"
+const Profile =() => {
+    return (
+        <PageLayout>
+        
+
+        <div className="text-black flex flex-col justify-center items-center">
+        
+        <div> Next js is suck and stupid </div>
+        
+        
+        
+        </div>
+        </ PageLayout> 
+    )
+}
+
+
+
+
+export default Profile
