01f6d9124ddcfec294f4e399c09010aac33aaf08
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-07-12 01:38:07 +0300

    Firebase config  move to secret

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -28,12 +28,12 @@ import store from '../app/store';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyB8UTCbWS3P2FHDpLXFG07yTQmalAa8EHw',
-	authDomain: 'crowdfundingcapstone.firebaseapp.com',
-	projectId: 'crowdfundingcapstone',
-	storageBucket: 'crowdfundingcapstone.appspot.com',
-	messagingSenderId: '312824414413',
-	appId: '1:312824414413:web:ad1d445759277801dc6984',
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
