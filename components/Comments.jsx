692f64774b2d98ef2726c22ee08835cf30aeb88b
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-07-20 01:05:29 +0300

    Feat
    Code refactoring. Added a new component for comments and a new component for social sharing. Also added a new component for deleting comments.

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,129 +1,81 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { db, auth, handleCommentDelete } from '@/firebase/firebase';
 import {
-  db,
-  createComment,
-  auth,
-  handleCommentDelete,
-} from "@/firebase/firebase";
-import {
-  collection,
-  onSnapshot,
-  query,
-  where,
-  getDocs,
-} from "firebase/firestore";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import Image from "next/legacy/image";
+	collection,
+	onSnapshot,
+	query,
+	where,
+	getDocs,
+} from 'firebase/firestore';
+import Image from 'next/legacy/image';
 const Comments = ({ projectId }) => {
-  const [comments, setComments] = useState([]);
-  useEffect(() => {
-    const commentsRef = collection(db, "comments");
-    const q = query(commentsRef, where("projectId", "==", projectId));
-    onSnapshot(q, async (querySnapshot) => {
-      const commentsData = [];
+	const [comments, setComments] = useState([]);
+	const [isDeleteCommentOpen, setIsDeleteCommentOpen] = useState(false);
+	useEffect(() => {
+		const commentsRef = collection(db, 'comments');
+		const q = query(commentsRef, where('projectId', '==', projectId));
+		const unsubscribe = onSnapshot(q, async (querySnapshot) => {
+			const commentsData = [];
+			for (const doc of querySnapshot.docs) {
+				const comment = doc?.data();
+				const commentId = doc?.id;
+				const usersCollection = collection(db, 'users');
+				const userQuery = query(
+					usersCollection,
+					where('uid', '==', comment.userUid)
+				);
+				const userQuerySnapshot = await getDocs(userQuery);
+				const userData = userQuerySnapshot.docs[0].data();
+				commentsData.push({ ...comment, user: userData, id: commentId });
+			}
+			commentsData.sort((a, b) => b.timestamp - a.timestamp);
+			setComments(commentsData);
+		});
 
-      for (const doc of querySnapshot.docs) {
-        const comment = doc.data();
-        const commentId = doc.id;
-        const usersCollection = collection(db, "users");
-        const userQuery = query(
-          usersCollection,
-          where("uid", "==", comment.userUid)
-        );
-        const userQuerySnapshot = await getDocs(userQuery);
-        const userData = userQuerySnapshot.docs[0].data();
+		return () => {
+			unsubscribe();
+		};
+	}, []);
 
-        commentsData.push({ ...comment, user: userData, id: commentId });
-      }
+	const openDeleteComment = () => {
+		setIsDeleteCommentOpen(true);
+	};
+	return (
+		<>
+			{isDeleteCommentOpen && (
+				<DeleteComment
+					setIsDeleteCommentOpen={setIsDeleteCommentOpen}
+					projectId={projectId}
+				/>
+			)}
+			<div className='flex flex-col'>
+				<div>
+					{comments?.map((comment) => (
+						<div key={comment?.timestamp}>
+							<div className='flex items-center'>
+								<Image
+									width={40}
+									height={40}
+									className='rounded-full'
+									alt='user image'
+									src={comment?.user?.photoURL}
+								/>
+								<p>{comment?.user?.displayName}, Said:</p>
+							</div>
 
-      commentsData.sort((a, b) => b.timestamp - a.timestamp);
-      setComments(commentsData);
-    });
-  }, []);
-
-  console.log(comments);
-  const schema = yup.object().shape({
-    comment: yup.string().trim().required("Comment is required"),
-  });
-  const {
-    handleSubmit,
-    reset,
-    register,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-  const onSubmit = (data, e) => {
-    e.preventDefault();
-    createComment({
-      projectId: projectId,
-      comment: data.comment,
-      timestamp: new Date(),
-      userUid: auth.currentUser.uid,
-    });
-    reset();
-    setComments([]);
-  };
-  const handleDelete = async (commentId) => {
-    await handleCommentDelete(commentId);
-  };
-  return (
-    <>
-      <div className="flex flex-col">
-        <div>
-          {comments.map((comment) => (
-            <div key={comment?.timestamp}>
-              <div className="flex items-center">
-                <Image
-                  width={40}
-                  height={40}
-                  className="rounded-full"
-                  alt="user image"
-                  src={comment?.user?.photoURL}
-                />
-                <p>{comment?.user?.displayName}, Said:</p>
-              </div>
-
-              <p>{comment?.comment}</p>
-              <p>{comment?.timestamp.toDate().toLocaleString()}</p>
-              {auth?.currentUser?.uid === comment.userUid ? (
-                <button onClick={() => handleDelete(comment.id)}>Delete</button>
-              ) : (
-                ""
-              )}
-            </div>
-          ))}
-        </div>
-      </div>
-      {auth.currentUser == null ? (
-        <p>In order to command you need to Login</p>
-      ) : (
-        <div className="flex">
-          <form className="flex" onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <textarea
-                {...register("comment")}
-                type="text"
-                placeholder="Add a comment"
-              />
-              <p
-                className={`text-red-700 px-3 ${
-                  errors.comment ? "" : "invisible"
-                }`}
-              >
-                {errors.comment?.message || "Placeholder"}
-              </p>
-            </div>
-            <div>
-              <button type="submit">Submit</button>
-            </div>
-          </form>
-        </div>
-      )}
-    </>
-  );
+							<p>{comment?.comment}</p>
+							<p>{comment?.timestamp?.toDate().toLocaleString()}</p>
+							{auth?.currentUser?.uid === comment?.userUid ? (
+								<button onClick={openDeleteComment}>Delete</button>
+							) : (
+								''
+							)}
+						</div>
+					))}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Comments;
---
+++ components/DeleteComment.jsx
@@ -0,0 +1,56 @@
+'use client';
+import React, { useState } from 'react';
+import { db, auth, handleCommentDelete } from '@/firebase/firebase';
+
+function DeleteComment({ setIsDeleteCommentOpen, projectId }) {
+	// const imageUrl = useSelector(()=>state.imageUrl.imageUrl)
+	const handleDeleteComment = async () => {
+		await handleDelete(projectId);
+		handleCommentDelete(false);
+	};
+	return (
+		<div className='fixed top-0 left-0 z-10 inset-0 overflow-y-auto w-full pt-16'>
+			<div className='flex items-center justify-center min-h-screen overflow-auto pt-4 px-4 pb-20 text-center'>
+				<div
+					className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'
+					aria-hidden='true'
+				></div>
+				<span
+					className='hidden sm:inline-block sm:align-middle sm:h-screen'
+					aria-hidden='true'
+				>
+					&#8203;
+				</span>
+				<div className='inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'>
+					<div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+						<div className='flex flex-col gap-12 '>
+							<div>
+								<p>Are You Sure that you want to delete this project ?</p>
+							</div>
+							<div className='flex justify-center items-center'>
+								<button
+									onClick={handleDeleteComment}
+									className='bg-red-400 text-white rounded m-2 p-2'
+								>
+									{' '}
+									Delete
+								</button>
+								<button
+									onClick={() => {
+										setIsDeleteCommentOpen(false);
+									}}
+									className=' rounded m-2 p-2 bg-transparent text-black'
+								>
+									{' '}
+									Cancel
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default DeleteComment;
