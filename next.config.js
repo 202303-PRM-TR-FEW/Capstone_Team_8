6032c3642acb1f4c39e3d19b9d9652fefc27701a
alwaysalty <cenkerdonmez@gmail.com>
2023-07-20 10:47:28 +0300

    Eslint config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ['firebasestorage.googleapis.com'],
-	},
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
