0fc22e321e81b487c564fe31a91adf16c09d02bb
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-07-22 22:23:23 +0300

    faqstyle

--- app/faq/page.js
+++ app/faq/page.js
@@ -1,40 +1,69 @@
-import React from 'react';
-import PageLayout from '@/components/PageLayout';
+import React from "react";
+import PageLayout from "@/components/PageLayout";
 
 function Faq() {
-	return <PageLayout>
-		<div className='flex justify-center items-center h-screen'>
-Q1: What is crowdfunding? <br />
-A: Crowdfunding is a method of raising funds for a project, business, or cause by collecting small contributions from a large number of people through an online platform. <br />
-
-Q2: How does crowdfunding work? <br />
-A: In crowdfunding, project creators set a funding goal and offer rewards or incentives to backers who contribute money to support the project. If the funding goal is reached within a specified timeframe, the project is funded, and backers receive their rewards. <br />
-
-Q3: What types of projects can I crowdfund? <br />
-A: Crowdfunding is versatile and can be used for various projects, including startups, creative ventures, charitable causes, product development, and more. It is essential to choose a platform that aligns with your project type. <br />
-
-Q4: Is crowdfunding safe? <br />
-A: Crowdfunding platforms prioritize security and safety. They use encrypted payment gateways to protect your financial information. However, backers should be aware that crowdfunding carries inherent risks, and projects may not always deliver as planned. <br />
-
-Q5: Can I get a refund if a project I backed does not succeed? <br />
-A: It depends on the crowdfunding platform and the projects specific terms. In some cases, backers may receive a refund if the funding goal is not met, while others might have a all-or-nothing policy. <br />
-
-Q6: How do I create a successful crowdfunding campaign? <br />
-A: To create a successful campaign, be clear about your projects purpose, set realistic funding goals, provide compelling rewards, and engage with your backers through regular updates. <br />
-
-Q7: Can I change my reward selection after I have backed a project? <br />
-A: Most crowdfunding platforms allow backers to change their reward selection before the campaign ends. Check the platforms guidelines for specific details. <br />
-
-Q8: What happens if a project I backed does not deliver as promised? <br />
-A: While crowdfunding platforms strive to ensure project legitimacy, there is always a possibility of delays or unforeseen challenges. If you encounter such issues, try to communicate with the project creator first. If necessary, contact the platforms support for further assistance. <br />
-
-Q9: Can I cancel my pledge during a crowdfunding campaign? <br />
-A: Yes, in most cases, backers can cancel their pledges before the campaign ends. However, double-check the platforms rules as they may vary. <br />
-
-Q10: How do I contact the project creator for more information? <br />
-A: On most crowdfunding platforms, there is a messaging system that allows backers to communicate with the project creator directly. You can use this feature to ask questions or seek additional information about the project. <br />
-</div>
-	</PageLayout>;
+  return (
+    <PageLayout>
+      <div className="flex flex-col justify-center items-center py-24 gap-4 sm:min-h-[95vh] min-h-[95svh] px-4 sm:px-6 ">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl">Q1: What is crowdfunding?</h2>
+          <p className="pl-2">
+            A: Crowdfunding is a method of raising funds for a project,
+            business, or cause by collecting small contributions from a large
+            number of people through an online platform.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl">Q2: How does crowdfunding work?</h2>
+          <p className="pl-2">
+            A: In crowdfunding, project creators set a funding goal and offer
+            rewards or incentives to backers who contribute money to support the
+            project. If the funding goal is reached within a specified
+            timeframe, the project is funded, and backers receive their rewards.
+          </p>
+        </div>
+        Q3: What types of projects can I crowdfund? <br />
+        A: Crowdfunding is versatile and can be used for various projects,
+        including startups, creative ventures, charitable causes, product
+        development, and more. It is essential to choose a platform that aligns
+        with your project type. <br />
+        Q4: Is crowdfunding safe? <br />
+        A: Crowdfunding platforms prioritize security and safety. They use
+        encrypted payment gateways to protect your financial information.
+        However, backers should be aware that crowdfunding carries inherent
+        risks, and projects may not always deliver as planned. <br />
+        Q5: Can I get a refund if a project I backed does not succeed? <br />
+        A: It depends on the crowdfunding platform and the projects specific
+        terms. In some cases, backers may receive a refund if the funding goal
+        is not met, while others might have a all-or-nothing policy. <br />
+        Q6: How do I create a successful crowdfunding campaign? <br />
+        A: To create a successful campaign, be clear about your projects
+        purpose, set realistic funding goals, provide compelling rewards, and
+        engage with your backers through regular updates. <br />
+        Q7: Can I change my reward selection after I have backed a project?{" "}
+        <br />
+        A: Most crowdfunding platforms allow backers to change their reward
+        selection before the campaign ends. Check the platforms guidelines for
+        specific details. <br />
+        Q8: What happens if a project I backed does not deliver as promised?{" "}
+        <br />
+        A: While crowdfunding platforms strive to ensure project legitimacy,
+        there is always a possibility of delays or unforeseen challenges. If you
+        encounter such issues, try to communicate with the project creator
+        first. If necessary, contact the platforms support for further
+        assistance. <br />
+        Q9: Can I cancel my pledge during a crowdfunding campaign? <br />
+        A: Yes, in most cases, backers can cancel their pledges before the
+        campaign ends. However, double-check the platforms rules as they may
+        vary. <br />
+        Q10: How do I contact the project creator for more information? <br />
+        A: On most crowdfunding platforms, there is a messaging system that
+        allows backers to communicate with the project creator directly. You can
+        use this feature to ask questions or seek additional information about
+        the project. <br />
+      </div>
+    </PageLayout>
+  );
 }
 
 export default Faq;
