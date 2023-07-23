430d9af8d3521ee7f04532b5aa9cc68f0b313ca4
alwaysalty <cenkerdonmez@gmail.com>
2023-07-23 14:22:28 +0300

    Feat
    multilanguage added to more pages and components

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,102 +1,102 @@
-import React from 'react';
-import { createComment, auth } from '@/firebase/firebase';
-import Image from 'next/legacy/image';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { useForm } from 'react-hook-form';
-import welcome from '@/public/welcome_mobile.png';
-import { comment } from 'postcss';
+import React from "react";
+import { createComment, auth } from "@/firebase/firebase";
+import Image from "next/legacy/image";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import welcome from "@/public/welcome_mobile.png";
+import { useTranslations } from "next-intl";
 const Comment = ({ projectId }) => {
-	const schema = yup.object().shape({
-		comment: yup.string().trim(),
-	});
-	const {
-		handleSubmit,
-		reset,
-		register,
-		setValue,
-		clearErrors,
-		watch,
-		formState: { errors },
-	} = useForm({
-		resolver: yupResolver(schema),
-	});
-	const comment = watch('comment', '');
-	const onSubmit = (data) => {
-		createComment({
-			projectId: projectId,
-			comment: data?.comment,
-			timestamp: new Date(),
-			userUid: auth?.currentUser?.uid,
-		});
-		reset();
-	};
-	const handleCancel = (e) => {
-		e.preventDefault();
-		setValue('comment', '');
-		clearErrors('comment');
-	};
+  const schema = yup.object().shape({
+    comment: yup.string().trim(),
+  });
+  const {
+    handleSubmit,
+    reset,
+    register,
+    setValue,
+    clearErrors,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const comment = watch("comment", "");
+  const onSubmit = (data) => {
+    createComment({
+      projectId: projectId,
+      comment: data?.comment,
+      timestamp: new Date(),
+      userUid: auth?.currentUser?.uid,
+    });
+    reset();
+  };
+  const handleCancel = (e) => {
+    e.preventDefault();
+    setValue("comment", "");
+    clearErrors("comment");
+  };
+  const t = useTranslations();
+  return (
+    <>
+      {auth?.currentUser == null ? (
+        <p>{t("In order to command you need to Login")}</p>
+      ) : (
+        <div className='flex my-6 shadow-lg p-4 '>
+          <form className='flex w-full ' onSubmit={handleSubmit(onSubmit)}>
+            <div className='flex-flex-col w-full'>
+              <div className='w-full flex items-center gap-4'>
+                <Image
+                  width={40}
+                  height={40}
+                  className='rounded-full'
+                  alt='user image'
+                  src={auth?.currentUser?.photoURL || welcome}
+                />
 
-	return (
-		<>
-			{auth?.currentUser == null ? (
-				<p>In order to command you need to Login</p>
-			) : (
-				<div className='flex my-6 shadow-lg p-4 '>
-					<form className='flex w-full ' onSubmit={handleSubmit(onSubmit)}>
-						<div className='flex-flex-col w-full'>
-							<div className='w-full flex items-center gap-4'>
-								<Image
-									width={40}
-									height={40}
-									className='rounded-full'
-									alt='user image'
-									src={auth?.currentUser?.photoURL || welcome}
-								/>
+                <div className='w-full'>
+                  <input
+                    className='border-b-2 border-black p-2 w-full focus:outline-none focus:shadow-outline'
+                    {...register("comment")}
+                    type='text'
+                    placeholder={t("Add a comment")}
+                  />
+                  <p
+                    className={`text-red-700 pt-2 px-2 ${
+                      errors.comment ? "" : "invisible"
+                    }`}
+                  >
+                    {errors.comment?.message || "Placeholder"}
+                  </p>
+                </div>
+              </div>
 
-								<div className='w-full'>
-									<input
-										className='border-b-2 border-black p-2 w-full focus:outline-none focus:shadow-outline'
-										{...register('comment')}
-										type='text'
-										placeholder='Add a comment'
-									/>
-									<p
-										className={`text-red-700 pt-2 px-2 ${
-											errors.comment ? '' : 'invisible'
-										}`}
-									>
-										{errors.comment?.message || 'Placeholder'}
-									</p>
-								</div>
-							</div>
-
-							<div className='flex justify-end gap-4 '>
-								<button
-									onClick={handleCancel}
-									className='p-2 rounded bg-red-400 text-white hover:bg-red-600 hover:dropshadow-lg'
-								>
-									Cancel
-								</button>{' '}
-								<button
-									className={`p-2 rounded text-white hover:dropshadow-lg 
+              <div className='flex justify-end gap-4 '>
+                <button
+                  onClick={handleCancel}
+                  className='p-2 rounded bg-red-400 text-white hover:bg-red-600 hover:dropshadow-lg'
+                >
+                  {t("Cancel")}
+                </button>{" "}
+                <button
+                  className={`p-2 rounded text-white hover:dropshadow-lg 
 		${
-			comment?.trim().length < 1
-				? 'bg-blue-200 cursor-not-allowed'
-				: 'bg-blue-400 hover:bg-blue-600'
-		}`}
-									type='submit'
-									disabled={comment?.trim().length < 1}
-								>
-									Submit
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			)}
-		</>
-	);
+      comment?.trim().length < 1
+        ? "bg-blue-200 cursor-not-allowed"
+        : "bg-blue-400 hover:bg-blue-600"
+    }`}
+                  type='submit'
+                  disabled={comment?.trim().length < 1}
+                >
+                  {t("Comment")}
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Comment;
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,88 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { query, onSnapshot, collection } from 'firebase/firestore';
-import { db } from '@/firebase/firebase';
-import { useSelector, useDispatch } from 'react-redux';
-import { setSearchResults } from '@/app/[locale]/features/searchproject/searchproject';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { query, onSnapshot, collection } from "firebase/firestore";
+import { db } from "@/firebase/firebase";
+import { useSelector, useDispatch } from "react-redux";
+import { setSearchResults } from "@/app/[locale]/features/searchproject/searchproject";
+import moment from "moment";
+import { useTranslations } from "next-intl";
 
 export default function SearchBar({ setIsSeachBarOpen }) {
-	const [text, setText] = useState('');
-	const [data, setData] = useState([]);
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+  const t = useTranslations();
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const searchText = (e) => {
+    e.preventDefault();
+    if (!text || text.trim().length < 1) {
+      return;
+    }
+    setIsSeachBarOpen(false);
+    filterDataBySearchQuery();
 
-	const router = useRouter();
-	const dispatch = useDispatch();
-	const searchText = (e) => {
-		e.preventDefault();
-		if (!text || text.trim().length < 1) {
-			return;
-		}
-		setIsSeachBarOpen(false);
-		filterDataBySearchQuery();
+    router.push("/search");
+    setText("");
+  };
+  useEffect(() => {
+    const q = query(collection(db, "app"));
+    onSnapshot(q, (querySnapshot) => {
+      let dataArr = [];
+      querySnapshot.forEach((doc) => {
+        const projectData = { ...doc.data(), id: doc.id };
 
-		router.push('/search');
-		setText('');
-	};
-	useEffect(() => {
-		const q = query(collection(db, 'app'));
-		onSnapshot(q, (querySnapshot) => {
-			let dataArr = [];
-			querySnapshot.forEach((doc) => {
-				const projectData = { ...doc.data(), id: doc.id };
+        const totalDonations = projectData.donations.reduce(
+          (total, donation) => total + parseInt(donation.donation),
+          0
+        );
+        dataArr.push({ ...projectData, totalDonations });
+      });
+      dataArr.sort((a, b) => b.totalDonations - a.totalDonations);
 
-				const totalDonations = projectData.donations.reduce(
-					(total, donation) => total + parseInt(donation.donation),
-					0
-				);
-				dataArr.push({ ...projectData, totalDonations });
-			});
-			dataArr.sort((a, b) => b.totalDonations - a.totalDonations);
-
-			setData(dataArr);
-			return dataArr;
-		});
-	}, []);
-	const filterDataBySearchQuery = () => {
-		const filteredResults = data.filter((item) =>
-			item.title.toLowerCase().includes(text.toLowerCase())
-		);
-		filteredResults.forEach((item) => {
-			item.endTime = item?.endTime?.seconds;
-		});
-		setSearchResults(filteredResults);
-		dispatch(setSearchResults(filteredResults));
-	};
-	const handleText = (e) => {
-		setText(e.target.value);
-	};
-	return (
-		<form onSubmit={searchText}>
-			<input
-				type='text'
-				value={text}
-				onChange={handleText}
-				id='search-navbar'
-				className='block lg:w-72 relative w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 '
-				placeholder='Search...'
-			/>
-			<button
-				type='submit'
-				className='absolute inset-y-0 top-0 right-5 flex items-center pl-3 '
-			>
-				<svg
-					className='w-5 h-5 cursor-pointer  text-gray-700 hover:scale-105'
-					aria-hidden='true'
-					fill='currentColor'
-					viewBox='0 0 20 20'
-					xmlns='http://www.w3.org/2000/svg'
-				>
-					<path
-						fillRule='evenodd'
-						d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
-						clipRule='evenodd'
-					></path>
-				</svg>
-			</button>
-		</form>
-	);
+      setData(dataArr);
+      return dataArr;
+    });
+  }, []);
+  const filterDataBySearchQuery = () => {
+    const filteredResults = data.filter((item) =>
+      item.title.toLowerCase().includes(text.toLowerCase())
+    );
+    filteredResults.forEach((item) => {
+      item.endTime = item?.endTime?.seconds;
+    });
+    setSearchResults(filteredResults);
+    dispatch(setSearchResults(filteredResults));
+  };
+  const handleText = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <form onSubmit={searchText}>
+      <input
+        type='text'
+        value={text}
+        onChange={handleText}
+        id='search-navbar'
+        className='block lg:w-72 relative w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 '
+        placeholder={t("Search")}
+      />
+      <button
+        type='submit'
+        className='absolute inset-y-0 top-0 right-5 flex items-center pl-3 '
+      >
+        <svg
+          className='w-5 h-5 cursor-pointer  text-gray-700 hover:scale-105'
+          aria-hidden='true'
+          fill='currentColor'
+          viewBox='0 0 20 20'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            fillRule='evenodd'
+            d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
+            clipRule='evenodd'
+          ></path>
+        </svg>
+      </button>
+    </form>
+  );
 }
