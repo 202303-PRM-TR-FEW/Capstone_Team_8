c287941ffdc9d96190e578425781b153e9e5d8cf
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-07-28 13:51:29 +0300

    finishedfaqtranslation

--- app/[locale]/faq/page.js
+++ app/[locale]/faq/page.js
@@ -69,9 +69,7 @@ function Faq() {
               className="w-full px-5 py-3 outline-none focus:outline-none"
               onClick={() => toggleSection("section3")}
             >
-              <p className="font-semibold text-left w-full ">
-                Q3: What types of projects can I crowdfund?
-              </p>
+              <p className="font-semibold text-left w-full ">{t("faq_q3")}</p>
             </button>
             {openSection == "section3" ? (
               <button className="text-2xl">-</button>
@@ -82,12 +80,7 @@ function Faq() {
 
           {openSection === "section3" && (
             <div className="p-5  border-gray-200 flex justify-start w-full border-t-2">
-              <p className="w-full text-left">
-                A: Crowdfunding is versatile and can be used for various
-                projects, including startups, creative ventures, charitable
-                causes, product development, and more. It is essential to choose
-                a platform that aligns with your project type.
-              </p>
+              <p className="w-full text-left">{t("faq_a3")}</p>
             </div>
           )}
         </div>
@@ -99,9 +92,7 @@ function Faq() {
               className="w-full px-5 py-3 outline-none focus:outline-none"
               onClick={() => toggleSection("section4")}
             >
-              <p className="font-semibold text-left w-full ">
-                Q4: Is crowdfunding safe?
-              </p>
+              <p className="font-semibold text-left w-full ">{t("faq_q4")}</p>
             </button>
             {openSection == "section4" ? (
               <button className="text-2xl">-</button>
@@ -112,13 +103,7 @@ function Faq() {
 
           {openSection === "section4" && (
             <div className="p-5  border-gray-200 flex justify-start w-full border-t-2">
-              <p className="w-full text-left">
-                A: Crowdfunding platforms prioritize security and safety. They
-                use encrypted payment gateways to protect your financial
-                information. However, backers should be aware that crowdfunding
-                carries inherent risks, and projects may not always deliver as
-                planned.
-              </p>
+              <p className="w-full text-left">{t("faq_a4")}</p>
             </div>
           )}
         </div>
@@ -130,9 +115,7 @@ function Faq() {
               className="w-full px-5 py-3 outline-none focus:outline-none"
               onClick={() => toggleSection("section5")}
             >
-              <p className="font-semibold text-left w-full ">
-                Q5: Can I get a refund if a project I backed does not succeed?
-              </p>
+              <p className="font-semibold text-left w-full ">{t("faq_q5")}</p>
             </button>
             {openSection == "section5" ? (
               <button className="text-2xl">-</button>
@@ -143,12 +126,7 @@ function Faq() {
 
           {openSection === "section5" && (
             <div className="p-5  border-gray-200 flex justify-start w-full border-t-2">
-              <p className="w-full text-left">
-                A: It depends on the crowdfunding platform and the projects
-                specific terms. In some cases, backers may receive a refund if
-                the funding goal is not met, while others might have a
-                all-or-nothing policy.
-              </p>
+              <p className="w-full text-left">{t("faq_a5")}</p>
             </div>
           )}
         </div>
@@ -160,9 +138,7 @@ function Faq() {
               className="w-full px-5 py-3 outline-none focus:outline-none"
               onClick={() => toggleSection("section6")}
             >
-              <p className="font-semibold text-left w-full ">
-                Q6: How do I create a successful crowdfunding campaign?
-              </p>
+              <p className="font-semibold text-left w-full ">{t("faq_q6")}</p>
             </button>
             {openSection == "section6" ? (
               <button className="text-2xl">-</button>
@@ -173,11 +149,7 @@ function Faq() {
 
           {openSection === "section6" && (
             <div className="p-5  border-gray-200 flex justify-start w-full border-t-2">
-              <p className="w-full text-left">
-                A: To create a successful campaign, be clear about your projects
-                purpose, set realistic funding goals, provide compelling
-                rewards, and engage with your backers through regular updates.
-              </p>
+              <p className="w-full text-left">{t("faq_a6")}</p>
             </div>
           )}
         </div>
@@ -189,10 +161,7 @@ function Faq() {
               className="w-full px-5 py-3 outline-none focus:outline-none"
               onClick={() => toggleSection("section7")}
             >
-              <p className="font-semibold text-left w-full ">
-                Q7: Can I change my reward selection after I have backed a
-                project?
-              </p>
+              <p className="font-semibold text-left w-full ">{t("faq_q7")}</p>
             </button>
             {openSection == "section7" ? (
               <button className="text-2xl">-</button>
@@ -203,12 +172,7 @@ function Faq() {
 
           {openSection === "section7" && (
             <div className="p-5  border-gray-200 flex justify-start w-full border-t-2">
-              <p className="w-full text-left">
-                A: Most crowdfunding platforms allow backers to change their
-                reward selection before the campaign ends. Check the platforms
-                guidelines for specific details and more information about our
-                campaign
-              </p>
+              <p className="w-full text-left">{t("faq_a7")}</p>
             </div>
           )}
         </div>
@@ -220,10 +184,7 @@ function Faq() {
               className="w-full px-5 py-3 outline-none focus:outline-none"
               onClick={() => toggleSection("section8")}
             >
-              <p className="font-semibold text-left w-full ">
-                Q8: What happens if a project I backed does not deliver as
-                promised?
-              </p>
+              <p className="font-semibold text-left w-full ">{t("faq_q8")}</p>
             </button>
             {openSection == "section8" ? (
               <button className="text-2xl">-</button>
@@ -234,13 +195,7 @@ function Faq() {
 
           {openSection === "section8" && (
             <div className="p-5  border-gray-200 flex justify-start w-full border-t-2">
-              <p className="w-full text-left">
-                A: While crowdfunding platforms strive to ensure project
-                legitimacy, there is always a possibility of delays or
-                unforeseen challenges. If you encounter such issues, try to
-                communicate with the project creator first. If necessary,
-                contact the platforms support for further assistance.
-              </p>
+              <p className="w-full text-left">{t("faq_a8")}</p>
             </div>
           )}
         </div>
@@ -252,9 +207,7 @@ function Faq() {
               className="w-full px-5 py-3 outline-none focus:outline-none"
               onClick={() => toggleSection("section9")}
             >
-              <p className="font-semibold text-left w-full ">
-                Q9: Can I cancel my pledge during a crowdfunding campaign?
-              </p>
+              <p className="font-semibold text-left w-full ">{t("faq_q9")}</p>
             </button>
             {openSection == "section9" ? (
               <button className="text-2xl">-</button>
@@ -265,12 +218,7 @@ function Faq() {
 
           {openSection === "section9" && (
             <div className="p-5  border-gray-200 flex justify-start w-full border-t-2">
-              <p className="w-full text-left">
-                A: No, in most cases, backers can not cancel their pledges
-                before the campaign ends. However, double-check the platforms
-                rules as they may vary. You may also contact us by email to
-                resolve your issues.
-              </p>
+              <p className="w-full text-left">{t("faq_a9")}</p>
             </div>
           )}
         </div>
@@ -282,9 +230,7 @@ function Faq() {
               className="w-full px-5 py-3 outline-none focus:outline-none"
               onClick={() => toggleSection("section10")}
             >
-              <p className="font-semibold text-left w-full ">
-                Q10: How do I contact the project creator for more information?
-              </p>
+              <p className="font-semibold text-left w-full ">{t("faq_q10")} </p>
             </button>
             {openSection == "section10" ? (
               <button className="text-2xl">-</button>
@@ -295,12 +241,7 @@ function Faq() {
 
           {openSection === "section10" && (
             <div className="p-5  border-gray-200 flex justify-start w-full border-t-2">
-              <p className="w-full text-left">
-                A: On most crowdfunding platforms, there is a messaging system
-                that allows backers to communicate with the project creator
-                directly. You can use this feature to ask questions or seek
-                additional information about the project.
-              </p>
+              <p className="w-full text-left">{t("faq_a10")}</p>
             </div>
           )}
         </div>
