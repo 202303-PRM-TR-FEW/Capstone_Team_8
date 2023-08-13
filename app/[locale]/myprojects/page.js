54c1cfee5c57783550e28e335fdfdccf3e00346a
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-08-13 09:33:12 +0300

    fix My project card revised

--- app/[locale]/myprojects/page.js
+++ app/[locale]/myprojects/page.js
@@ -1,127 +1,127 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { query, collection, onSnapshot } from "firebase/firestore";
-import { db } from "../../../firebase/firebase";
-import { getAuth } from "@firebase/auth";
-import { useSelector, useDispatch } from "react-redux";
-import KickOffProject from "@/components/KickOffProject";
-import WithAuth from "@/components/AuthanticatedRoute";
-import Loading from "@/app/[locale]/loading";
-import MyProjectsCard from "@/components/MyProjectsCard";
-import { openAddProject } from "../features/startproject/kickoff";
-import { useTranslations, useLocale } from "next-intl";
-import Link from "next-intl/link";
-import ScrollToTop from "@/components/ScrollToTop";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { query, collection, onSnapshot } from 'firebase/firestore';
+import { db } from '../../../firebase/firebase';
+import { getAuth } from '@firebase/auth';
+import { useSelector, useDispatch } from 'react-redux';
+import KickOffProject from '@/components/KickOffProject';
+import WithAuth from '@/components/AuthanticatedRoute';
+import Loading from '@/app/[locale]/loading';
+import MyProjectsCard from '@/components/MyProjectsCard';
+import { openAddProject } from '../features/startproject/kickoff';
+import { useTranslations, useLocale } from 'next-intl';
+import Link from 'next-intl/link';
+import ScrollToTop from '@/components/ScrollToTop';
 
 function MyProject(props) {
-  const locale = useLocale();
-  const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
+	const locale = useLocale();
+	const [data, setData] = useState([]);
+	const [filteredData, setFilteredData] = useState([]);
 
-  const kickOffModalStatus = useSelector(
-    (state) => state.isStartProjectOpen.modalOpen
-  );
-  const auth = getAuth();
-  const dispatch = useDispatch();
-  const t = useTranslations();
-  useEffect(() => {
-    const q = query(collection(db, "app"));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let dataArr = [];
-      querySnapshot.forEach((doc) => {
-        const projectData = { ...doc.data(), id: doc.id };
-        const totalDonations = projectData.donations.reduce(
-          (total, donation) => total + parseInt(donation.donation),
-          0
-        );
-        dataArr.push({ ...projectData, totalDonations });
-      });
-      dataArr.sort((a, b) => b.totalDonations - a.totalDonations);
+	const kickOffModalStatus = useSelector(
+		(state) => state.isStartProjectOpen.modalOpen
+	);
+	const auth = getAuth();
+	const dispatch = useDispatch();
+	const t = useTranslations();
+	useEffect(() => {
+		const q = query(collection(db, 'app'));
+		const unsubscribe = onSnapshot(q, (querySnapshot) => {
+			let dataArr = [];
+			querySnapshot.forEach((doc) => {
+				const projectData = { ...doc.data(), id: doc.id };
+				const totalDonations = projectData.donations.reduce(
+					(total, donation) => total + parseInt(donation.donation),
+					0
+				);
+				dataArr.push({ ...projectData, totalDonations });
+			});
+			dataArr.sort((a, b) => b.totalDonations - a.totalDonations);
 
-      setData(dataArr);
-    });
-    return () => unsubscribe();
-  }, []);
+			setData(dataArr);
+		});
+		return () => unsubscribe();
+	}, []);
 
-  useEffect(() => {
-    filterProjectByUserId();
-  }, [data]);
+	useEffect(() => {
+		filterProjectByUserId();
+	}, [data]);
 
-  const filterProjectByUserId = () => {
-    const filteredProjects =
-      data && data?.length > 0
-        ? data?.filter((item) => item.userId === auth?.currentUser?.uid)
-        : [];
-    setFilteredData(filteredProjects);
-    return filteredData;
-  };
-  if (props.loading || !props.user)
-    return (
-      <div>
-        <Loading></Loading>
-      </div>
-    );
+	const filterProjectByUserId = () => {
+		const filteredProjects =
+			data && data?.length > 0
+				? data?.filter((item) => item.userId === auth?.currentUser?.uid)
+				: [];
+		setFilteredData(filteredProjects);
+		return filteredData;
+	};
+	if (props.loading || !props.user)
+		return (
+			<div>
+				<Loading></Loading>
+			</div>
+		);
 
-  return (
-    <>
-      {kickOffModalStatus && <KickOffProject />}
-      <ScrollToTop />
-      <div className='flex  flex-col  w-full overflow-auto  sm:pt-24 pt-12  pb-20 '>
-        {filteredData.length < 1 ? (
-          <div
-            id='noData'
-            className='flex flex-col w-full h-full justify-end items-center gap-4 p-6'
-          >
-            <div>
-              <p className='font-bold text-3xl text-black '>
-                {t("no_project")}
-              </p>
-            </div>
+	return (
+		<>
+			{kickOffModalStatus && <KickOffProject />}
+			<ScrollToTop />
+			<div className='flex  flex-col  w-full overflow-auto  sm:pt-24 pt-12  pb-20 '>
+				{filteredData.length < 1 ? (
+					<div
+						id='noData'
+						className='flex flex-col w-full h-full justify-end items-center gap-4 p-6'
+					>
+						<div>
+							<p className='font-bold text-3xl text-black '>
+								{t('no_project')}
+							</p>
+						</div>
 
-            <div className='flex gap-4 justify-center items-center'>
-              <button
-                onClick={() => {
-                  dispatch(openAddProject());
-                }}
-              >
-                <Link
-                  className='block py-2 pl-3 pr-4 bg-gray-900 text-white rounded  hover:drop-shadow-xl hover:text-[#d4ee26]  '
-                  href='/projects'
-                  locale={locale}
-                >
-                  {t("start_project")}
-                </Link>
-              </button>
-            </div>
-          </div>
-        ) : (
-          <>
-            <div className='flex flex-col md:justify-start md:items-start justify-center items-center sm:gap-4 sm:p-6 p-2'>
-              <div>
-                <h1 className='font-bold sm:text-3xl text-lg py-2'>
-                  {t("your_projects")}
-                </h1>
-              </div>
-              <div>
-                <p className='font-bold sm:text-lg text-sm'>
-                  {t("your_projects_desc")}
-                </p>
-              </div>
-            </div>
-            <div className='flex flex-wrap md:justify-start md:items-start justify-center items-center w-full px-2 gap-2'>
-              {filteredData.map((project) => {
-                return (
-                  <div key={project.id} className=' flex justify-center  '>
-                    <MyProjectsCard project={project}></MyProjectsCard>
-                  </div>
-                );
-              })}
-            </div>
-          </>
-        )}
-      </div>
-    </>
-  );
+						<div className='flex gap-4 justify-center items-center'>
+							<button
+								onClick={() => {
+									dispatch(openAddProject());
+								}}
+							>
+								<Link
+									className='block py-2 pl-3 pr-4 bg-gray-900 text-white rounded  hover:drop-shadow-xl hover:text-[#d4ee26]  '
+									href='/projects'
+									locale={locale}
+								>
+									{t('start_project')}
+								</Link>
+							</button>
+						</div>
+					</div>
+				) : (
+					<>
+						<div className='flex flex-col md:justify-start md:items-start justify-center items-center sm:gap-4 sm:p-6 pt-12 p-2'>
+							<div>
+								<h1 className='font-bold sm:text-3xl text-center text-lg py-2'>
+									{t('your_projects')}
+								</h1>
+							</div>
+							<div>
+								<p className='font-bold sm:text-lg text-sm text-center'>
+									{t('your_projects_desc')}
+								</p>
+							</div>
+						</div>
+						<div className='flex flex-wrap md:justify-start md:items-start justify-center items-center w-full px-2 gap-2'>
+							{filteredData.map((project) => {
+								return (
+									<div key={project.id} className=' flex justify-center  '>
+										<MyProjectsCard project={project}></MyProjectsCard>
+									</div>
+								);
+							})}
+						</div>
+					</>
+				)}
+			</div>
+		</>
+	);
 }
 
 export default WithAuth(MyProject);
